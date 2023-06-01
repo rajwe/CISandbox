d6685e0c1893f68db66bffc619684d13d95554d4
Raj <raj.balaiyan@wetransfer.com>
2023-06-01 23:40:28 +0100

    Initial Commit

---
+++ CISandbox/CISandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CISandboxApp.swift
+//  CISandbox
+//
+//  Created by Raj Balaiyan on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CISandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
